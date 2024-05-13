e423b3910c9f9bb8e10f5bf17b22a7ac90132b01
Inteonsc <7150288+Inteonsc@users.noreply.github.com.>
2024-05-14 00:08:35 +0100

    Two Crystal Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jmpAmount = Math.floor(Math.sqrt(breaks.length))
+    
+    let amountJumped = jmpAmount;
+    for(;amountJumped < breaks.length; amountJumped+= jmpAmount){
+        if(breaks[amountJumped]){
+            break;
+        }
+    }
+    amountJumped -= jmpAmount
+    for(let j = 0; j<= jmpAmount && amountJumped< breaks.length; j++){
+        if(breaks[j + amountJumped]){
+            return j + amountJumped;
+        }
+    }
+
+    return -1;
 
 }
