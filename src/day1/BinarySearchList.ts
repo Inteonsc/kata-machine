e623a8d56b2df4511668fb9f4365032141e4bee0
Inteonsc <7150288+Inteonsc@users.noreply.github.com.>
2024-05-13 23:59:07 +0100

    Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let high = haystack.length;
+    let low = 0;
+    while (low < high){
+        let mid = Math.floor(low + (high - low) / 2);
+        let value = haystack[mid];
+        if(value === needle){
+            return true;
+        }
+        else if(value > needle){
+            high = mid
+        }else if (value < needle){
+            low = mid + 1
+        }
 
+    }
+    return false;
 }
