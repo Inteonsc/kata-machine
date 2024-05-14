d64c2e7639352fa15db18ca4e84ffacbf081db8a
Inteonsc <7150288+Inteonsc@users.noreply.github.com.>
2024-05-14 14:00:53 +0100

    Quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,36 @@
-export default function quick_sort(arr: number[]): void {
 
+
+function qs(arr: number[], low: number, high: number): void {
+    //base case
+    if(low >= high){
+        return;
+    }
+    const pivotIndex = partition(arr, low, high);
+
+    qs(arr, low, pivotIndex - 1)
+    qs(arr, pivotIndex +1, high)
+}
+
+function partition(arr: number[], low: number, high: number): number{
+    const pivot = arr[high] //can change to something else
+
+    let index = low - 1;
+
+    for(let i = low; i< high; ++i){
+        if(arr[i] <= pivot){
+            index += 1
+            const tmp = arr[i]
+            arr[i] = arr[index];
+            arr[index] = tmp;
+        }
+    }
+    index++;
+    arr[high] = arr[index];
+    arr[index] = pivot;
+
+    return index;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
